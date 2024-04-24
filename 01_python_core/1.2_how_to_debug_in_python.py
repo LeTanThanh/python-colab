678f5afe503d69757f1f7594552cb6411d19b9b3
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-25 00:13:47 +0700

    1.2 How to debug om Python?

---
+++ 01_python_core/1.2_how_to_debug_in_python.py
@@ -0,0 +1,11 @@
+import logging
+import pdb
+
+if __name__ == "__main__":
+  # logging.info("Hello Python")
+
+  print("This is 1st step")
+  var = "Hello, Python"
+  pdb.set_trace()
+  print("This is 2nd step")
+  print("This is 3rd step")
